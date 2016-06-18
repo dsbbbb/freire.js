021996d257a3be332c1ce5cdc7d9e445acac7ac4
dsbbbb <dsb510@gmail.com>
2016-06-17 20:39:16 -0400

    Create constructorFunctions.js

---
+++ constructorFunctions.js
@@ -0,0 +1,13 @@
+//Constructor function that makes this an element that also has
+
+  {
+    - text string
+    - an imag, video, embed, e or element behind it-flip card style
+    - addittional links--for related urls (pdfs, videos, etc), which will be styled with style options through css/less
+    - optional embed of constellation (which would be created by the/an other major constructor function
+    
+
+function ideologicalNode ( textString, {image, video, embed code}, additionalLinks, constellationName){
+
+
+}
